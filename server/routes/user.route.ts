d2a7ba1534b4e245144fdc66e00bf0030a361bfc
Satyajit <sahoosatyajit2801@gmail.com>
2023-12-14 01:21:37 +0530

    added social auth func

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -7,16 +7,22 @@ import {
   authorization,
   updateAccessToken,
   getUserInfo,
+  socialAuth,
 } from "../controllers/user.controller";
 import { isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registrationUser);
+
 userRouter.post("/activate-user", activateUser);
+
 userRouter.post("/login", loginUser);
+
 userRouter.get("/logout", isAuthenticated, logoutUser);
 
 userRouter.get("/refreshtoken", updateAccessToken);
+
 userRouter.get("/me", isAuthenticated, getUserInfo);
+userRouter.post("/social-auth", socialAuth);
 
 export default userRouter;
