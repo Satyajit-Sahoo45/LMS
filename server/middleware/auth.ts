23a72efadd30f75a8bec1248ab843feb494f60bf
Satyajit <sahoosatyajit2801@gmail.com>
2023-12-18 02:30:56 +0530

    added authorize role func

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -33,3 +33,19 @@ export const isAuthenticated = CatchAsyncError(
     next();
   }
 );
+
+// validate user role
+// validate user role
+export const authorizeRoles = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user?.role || "")) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user?.role} is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
