4f2f8d41598c9b6c3ec6f2a3cf8dfeab385758db
Satyajit <sahoosatyajit2801@gmail.com>
2023-12-21 01:22:17 +0530

    added order routes

--- server/app.ts
+++ server/app.ts
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
+import orderRouter from "./routes/order.route";
 import bodyParser from "body-parser";
 
 // body parser
@@ -24,6 +25,7 @@ app.use(
 // route
 app.use("/api/v1", userRouter);
 app.use("/api/v1", courseRouter);
+app.use("/api/v1", orderRouter);
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
