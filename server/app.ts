349c291c99778f731a5bb774dd92d7ef6e69f30a
Satyajit <sahoosatyajit2801@gmail.com>
2023-12-24 22:58:04 +0530

    fetch all notification for the admin only

--- server/app.ts
+++ server/app.ts
@@ -8,6 +8,7 @@ import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
 import bodyParser from "body-parser";
+import notificationRouter from "./routes/notification.route";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -26,6 +27,7 @@ app.use(
 app.use("/api/v1", userRouter);
 app.use("/api/v1", courseRouter);
 app.use("/api/v1", orderRouter);
+app.use("/api/v1", notificationRouter);
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
