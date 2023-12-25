92d4e5a7f0218e88aaf646f3f9330974b1e7ca35
Satyajit <sahoosatyajit2801@gmail.com>
2023-12-25 08:46:09 +0530

    added update user role functionality

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -13,7 +13,11 @@ import {
   sendToken,
 } from "../utils/jwt";
 import { redis } from "../utils/redis";
-import { getAllUsersServices, getUserById } from "../services/user.service";
+import {
+  getAllUsersServices,
+  getUserById,
+  updateUserRoleService,
+} from "../services/user.service";
 import cloudinary from "cloudinary";
 
 // register user
@@ -429,6 +433,18 @@ export const getAllUsers = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       getAllUsersServices(res);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// update user role --only for admin
+export const updateUserRole = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id, role } = req.body;
+      updateUserRoleService(res, id, role);
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));
     }
