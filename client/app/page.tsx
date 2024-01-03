c29737b8d661b68f2c24761d58c59e0ab6f0b722
Satyajit <sahoosatyajit2801@gmail.com>
2024-01-04 00:06:35 +0530

    added navbar and theme-switcher

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,10 +2,13 @@
 
 import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
+import Header from "./components/Header";
 
 interface Props {}
 
 const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
   return (
     <div>
       <Heading
@@ -13,6 +16,7 @@ const Page: FC<Props> = (props) => {
         description="LMS is a platform for student to learn and get help from teachers"
         keywords="Programming, MERN, REDUX"
       />
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
     </div>
   );
 };
