c0291c8cd7b00dae943b3bfbb617622b42a623de
Satyajit <sahoosatyajit2801@gmail.com>
2024-02-04 17:55:28 +0530

    added verification UI

--- client/app/components/Auth/Verification.tsx
+++ client/app/components/Auth/Verification.tsx
@@ -1,3 +1,4 @@
+import { styles } from "../../../app/styles/style";
 import React, { FC, useRef, useState } from "react";
 import { toast } from "react-hot-toast";
 import { VscWorkspaceTrusted } from "react-icons/vsc";
@@ -30,7 +31,7 @@ const Verification: FC<Props> = ({ setRoute }) => {
   });
 
   const verificationHandler = async () => {
-    console.log("text");
+    setInvalidError(true);
   };
 
   const handleInputChange = (index: number, value: string) => {
@@ -45,7 +46,54 @@ const Verification: FC<Props> = ({ setRoute }) => {
     }
   };
 
-  return <div>Verification</div>;
+  return (
+    <div>
+      <h1 className={`${styles.title}`}>Verify Your Account</h1>
+      <br />
+      <div className="w-full flex items-center justify-center mt-2">
+        <div className="w-[80px] h-[80px] rounded-full bg-[#497DF2] flex items-center justify-center">
+          <VscWorkspaceTrusted size={40} />
+        </div>
+      </div>
+      <br />
+      <br />
+      <div className="m-auto flex items-center justify-around">
+        {Object.keys(verifyNumber).map((key, index) => (
+          <input
+            type="number"
+            key={key}
+            ref={inputRefs[index]}
+            className={`w-[65px] h-[65px] bg-transparent border-[3px] rounded-[10px] flex items-center text-black dark:text-white justify-center text-[18px] font-sans outline-none text-center ${
+              invalidError
+                ? "shake border-red-500"
+                : "dark:border-white border-[#0000004a]"
+            }`}
+            placeholder=""
+            maxLength={1}
+            value={verifyNumber[key as keyof VerifyNumber]}
+            onChange={(e) => handleInputChange(index, e.target.value)}
+          />
+        ))}
+      </div>
+      <br />
+      <br />
+      <div className="w-full flex justify-center">
+        <button className={`${styles.button}`} onClick={verificationHandler}>
+          Verify OTP
+        </button>
+      </div>
+      <br />
+      <h5 className="text-center pt-4 font-sans text-[14px] text-black dark:text-white">
+        Go back to sign in?{" "}
+        <span
+          className="text-[#2190ff] pl-1 cursor-pointer"
+          onClick={() => setRoute("Login")}
+        >
+          Sign in
+        </span>
+      </h5>
+    </div>
+  );
 };
 
 export default Verification;
